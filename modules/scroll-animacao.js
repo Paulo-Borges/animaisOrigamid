7ea4ac756f5bd638491177f66605291145166e00
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-04-29 03:07:47 -0300

    feat: Update module

---
+++ modules/scroll-animacao.js
@@ -0,0 +1,29 @@
+export default function initAnimacaoScroll() {
+
+    const sections = document.querySelectorAll('[data-anime="scroll"]')
+    if(sections.length) {
+    
+    const windowMetade = window.innerHeight * 0.6
+    
+    function animaScroll() {
+        sections.forEach((section) => {
+            const sectionTop = section.getBoundingClientRect().top
+            const isSectionVisible = (sectionTop - windowMetade) < 0
+            if(isSectionVisible)
+                section.classList.add('ativo')
+            else 
+            section.classList.remove('ativo')
+        })
+    }
+    
+    animaScroll()
+    
+    window.addEventListener('scroll', animaScroll)
+    }
+    
+    }
+    
+    
+    // Adicione um atributo data-anime="show-down" e
+    // data-anime="show-right" a todos as section's
+    // com descricão dos animais.
