2f5607632de685e6cded970b94a1b2de6ac74f9f
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-05-15 04:55:32 -0300

    feat: Update outsideClick

--- modules/dropdwn-menu.js
+++ modules/dropdwn-menu.js
@@ -1,30 +1,25 @@
+import outsideClick from './outsideClick.js'
+
 export default function initDropdownMenu() {
-
-}
-
-const dropdownMenus = document.querySelectorAll('[data-dropdown]')
+    const dropdownMenus = document.querySelectorAll('[data-dropdown]')
 
 
-dropdownMenus.forEach(menu => {
-
-    ['touchstart', 'click'].forEach(userEvent => {
-        menu.addEventListener(userEvent, handleClick)
+    dropdownMenus.forEach(menu => {
+    
+        ['touchstart', 'click'].forEach(userEvent => {
+            menu.addEventListener(userEvent, handleClick)
+        })
     })
-})
-
-function handleClick(event) {
-    event.preventDefault()
-    this.classList.add('active')
-    outsideClick(this, () => {
-        this.classList.remove('active')
-    })
-}
-
-function outsideClick(element, callback) {
-    const html = document.documentElement
-    html.addEventListener('click', handleOutsideClick)
-    function handleOutsideClick(event) {
-        console.log(element)
-        callback()
+    
+    function handleClick(event) {
+        event.preventDefault()
+        this.classList.add('active')
+        outsideClick(this, ['touchstart', 'click'], () => {
+            this.classList.remove('active')
+        })
     }
 }
+
+
+
+
